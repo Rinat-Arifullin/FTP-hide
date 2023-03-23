9b49eb9bc0dea27596560f1363b15bbb19950df2
arifullinrd <arifullinrd@lad24.ru>
2023-03-23 18:52:06 +0300

    Тест

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import React from 'react';
+import React from "react";
 
-function App
-
-(
+function App(
 
 
 
-)
-
-
-{
-  return (
-    <div className="App">
-    </div>
-  );
+) {
+  return <div className="App"></div>;
 }
 
 export default App;
